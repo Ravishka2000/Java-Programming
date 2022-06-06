72da8b705e5423e35afe5bd5ea599c6884168b53
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2022-06-06 21:52:07 +0530

    Delete Online Exam Papers 2022/May_7_2021 directory

--- Online Exam Papers 2022/May_7_2021/MainApp.java
+++
@@ -1,7 +0,0 @@
-
-public class MainApp {
-    public static void main(String[] args) {
-        Weekend student1 = new Weekend();
-        student1.addDetails("Jane","Malabe",'F',1990,"DS","QA");
-    }
-}
