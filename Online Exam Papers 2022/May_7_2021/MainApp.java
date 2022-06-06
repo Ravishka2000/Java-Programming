5aa6f1363258b211cb0ecfcb79ac78473474e76f
Ravishka2000 <gamageravishkadulshan2000@gmail.com>
2022-06-06 21:48:58 +0530

    new

--- Online Exam Papers 2022/May_7_2021/MainApp.java
+++ Online Exam Papers 2022/May_7_2021/MainApp.java
@@ -2,5 +2,6 @@
 public class MainApp {
     public static void main(String[] args) {
         Weekend student1 = new Weekend();
+        student1.addDetails("Jane","Malabe",'F',1990,"DS","QA");
     }
 }
